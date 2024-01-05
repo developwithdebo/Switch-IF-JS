0ee23c765448f4b4439422ba7bde39b0eb9a63c3
Adesanya Abdullateef Adebowale <debo@abdullateet.dev>
2024-01-05 22:58:29 +0100

    Add files via upload

---
+++ siwth if.js
@@ -0,0 +1,17 @@
+var home = "ilorin";
+switch (home) {
+    case "ilorin":
+        console.log("you are correct!")
+        break;
+    case "oyo":
+        console.log("Try again later");
+        break;
+    case "offa":
+        console.log("a bit close!")
+        break;
+    case "isin":
+        console.log("close!")
+        break;
+    default:
+        console.log("none of the above");
+}
